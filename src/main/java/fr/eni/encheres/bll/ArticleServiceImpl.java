4768c170fbebc533c1072541efaa6cfb68cf421e
Houcine <houcinehannoun@gmail.com>
2025-07-01 15:04:22 +0200

    ajout ArticleServiceImpl

---
+++ src/main/java/fr/eni/encheres/bll/ArticleServiceImpl.java
@@ -0,0 +1,37 @@
+package fr.eni.encheres.bll;
+
+import fr.eni.encheres.bo.Article;
+
+import java.util.List;
+
+public class ArticleServiceImpl implements ArticleService {
+    @Override
+    public void vendreArticle(Article article) {
+
+    }
+
+    @Override
+    public void annulerVente(int idArticle) {
+
+    }
+
+    @Override
+    public List<Article> consulterArticles() {
+        return List.of();
+    }
+
+    @Override
+    public List<Article> consulterArticlesParNom(String nomArticle) {
+        return List.of();
+    }
+
+    @Override
+    public List<Article> consulterArticlesParCategorie(String idCategorie) {
+        return List.of();
+    }
+
+    @Override
+    public List<Article> consulterArticlesParEtat(String etatVente) {
+        return List.of();
+    }
+}
