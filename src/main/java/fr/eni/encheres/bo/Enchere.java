1fabcf0da13b0f939e3f395cc00a9229adc1c871
IssaDev99 <zawarudo74u@gmail.com>
2025-07-01 09:55:27 +0200

    coucheBO

---
+++ src/main/java/fr/eni/encheres/bo/Enchere.java
@@ -0,0 +1,78 @@
+package fr.eni.encheres.bo;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Enchere {
+    private LocalDate dateEnchere;
+    private int montantEnchere;
+
+    private Utilisateur utilisateur;
+    private Article article;
+
+    public Enchere() {
+    }
+
+    public Enchere(LocalDate dateEnchere, int montantEnchere, Utilisateur utilisateur, Article article) {
+        this.dateEnchere = dateEnchere;
+        this.montantEnchere = montantEnchere;
+        this.utilisateur = utilisateur;
+        this.article = article;
+    }
+
+    public LocalDate getDateEnchere() {
+        return dateEnchere;
+    }
+
+    public void setDateEnchere(LocalDate dateEnchere) {
+        this.dateEnchere = dateEnchere;
+    }
+
+    public int getMontantEnchere() {
+        return montantEnchere;
+    }
+
+    public void setMontantEnchere(int montantEnchere) {
+        this.montantEnchere = montantEnchere;
+    }
+
+    public Utilisateur getUtilisateur() {
+        return utilisateur;
+    }
+
+    public void setUtilisateur(Utilisateur utilisateur) {
+        this.utilisateur = utilisateur;
+    }
+
+    public Article getArticle() {
+        return article;
+    }
+
+    public void setArticle(Article article) {
+        this.article = article;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Enchere{");
+        sb.append("dateEnchere=").append(dateEnchere);
+        sb.append(", montantEnchere=").append(montantEnchere);
+        sb.append(", utilisateur=").append(utilisateur);
+        sb.append(", article=").append(article);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Enchere enchere = (Enchere) o;
+        return montantEnchere == enchere.montantEnchere && Objects.equals(dateEnchere, enchere.dateEnchere) && Objects.equals(utilisateur, enchere.utilisateur) && Objects.equals(article, enchere.article);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(dateEnchere, montantEnchere, utilisateur, article);
+    }
+}
+
