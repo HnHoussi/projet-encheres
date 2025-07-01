60f12de0a81d12b788c0f8e4fcfd7a83604b3b19
IssaDev99 <zawarudo74u@gmail.com>
2025-07-01 14:07:55 +0200

    EnchereService

--- src/main/java/fr/eni/encheres/bll/EnchereService.java
+++ src/main/java/fr/eni/encheres/bll/EnchereService.java
@@ -6,7 +6,10 @@ import java.util.List;
 
 public interface EnchereService {
     public Enchere faireEnchere(int idArticle);
-    public List<Enchere> listeEncheres();
+    public List<Enchere> consulterEncheres();
+    public List<Enchere> consulterEncheresParEtat(String etatVente);
     public void annulerEnchere(Enchere enchere);
+    public Enchere consulterEnchereparNom(String nomArticle);
+    public Enchere consulterEnchereparCategorie(int idEnchere);
 
 }
