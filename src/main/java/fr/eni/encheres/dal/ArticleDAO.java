1183eeaf8868c32a0ef61da042950f0bfbbfa0b3
Houcine <houcinehannoun@gmail.com>
2025-07-02 12:16:31 +0200

    bug correction

--- src/main/java/fr/eni/encheres/dal/ArticleDAO.java
+++ src/main/java/fr/eni/encheres/dal/ArticleDAO.java
@@ -8,7 +8,7 @@ public interface ArticleDAO {
     void create(Article article, long idUtilisateur);
     void delete(long  idArticle);
     List<Article> findAll();
-    List<Article> findByNom(String nomArticle);
+    List<Article> findByMotCles(String nomArticle);
     List<Article> findByCategorie(String idCategorie);
     List<Article> findByEtat(String etatVente);
 
